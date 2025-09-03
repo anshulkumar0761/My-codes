46a857c262953f636e23ec18e168a50d8d93e19e
unknown <anshulkumarshah@gmail.com>
2025-09-04 00:29:28 +0530

    first commit

--- hello.c
+++ hello.c
@@ -1,8 +1,13 @@
 #include <stdio.h>
 int main() {
-    printf ("heelloo\n");
-    printf ("heelloo\n");
-    printf ("heelloo\n");
- printf ("heelloo\n");
+    int n ;
+    int factorial , i;
+    printf("Enter the number ");
+    scanf("%d",&n);
+        for (i = 1; i <= n; i++) {
+            factorial = factorial*i;
+        }
+        printf("Factorial of %d = %d\n", n, factorial);
+    
     return 0;
 }

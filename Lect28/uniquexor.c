e410300ea8ae01d775b3c6fe73d2307ec28112a7
unknown <anshulkumarshah@gmail.com>
2025-11-05 12:07:43 +0530

    logicgates

---
+++ Lect28/uniquexor.c
@@ -0,0 +1,99 @@
+// #include<stdio.h>
+// int main(){
+//     int arr[5]={1,2,3,3,1};
+//     int xor = 0;
+//     for (int i = 0; i < 5 ; i++)
+//     {
+//        xor=xor^arr[i];
+//     }
+//     printf("%d",xor);
+// }
+
+
+
+
+// #include <stdio.h>
+// int main () {
+//     int a = 5 ;
+//     int b = 3 ;
+//     a = a ^ b ;
+//     b = a ^ b ;
+//     a = a ^ b ;
+//     printf("a : %d \n", a);
+//     printf("b : %d", b);
+
+// }
+
+// #include<stdio.h>
+// int main (){
+//     int a = 12 ;
+//     int leftshift = a<<1 ;
+//     int rightshift = a>>1 ;
+//     printf("Leftshift = %d \n",leftshift);
+//     printf("Rightshift = %d \n ",rightshift);
+
+// }
+// #include<stdio.h>
+// int main (){
+//     int arr[5]={1,2,4,5};
+//     int xor=0 , xor1 = 0;
+//     for (int i = 1; i <= 5 ; i++)
+//     {
+//         xor=xor^i;
+//     }
+//     for (int i = 0; i < 4; i++)
+//     {
+//         xor1=xor1^arr[i];
+//     }
+//  printf("%d", xor^xor1);   
+// }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
